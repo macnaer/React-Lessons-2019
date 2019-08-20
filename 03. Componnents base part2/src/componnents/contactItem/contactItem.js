07776a2096d43dd663e00b9abf02f4021e75dfc2
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2019-08-20 20:54:05 +0300

    onClick event and state

--- 03. Componnents base part2/src/componnents/contactItem/contactItem.js
+++ 03. Componnents base part2/src/componnents/contactItem/contactItem.js
@@ -1,11 +1,49 @@
 import React from 'react';
 
 class ContactItem extends React.Component{
-  
+
+    state = {
+        clicked: false,
+        btnContectText: "Contact Now",
+        avatar: this.props.avatar
+
+    }
+
+    onAvatar = () => {
+        const ava = 99;
+        this.setState({
+            avatar: ava
+        })
+    }
+
+    onContactClick = () => {
+        console.log("Contact to ", this.props.contactName);
+        if (!this.state.clicked){
+            this.setState({
+                clicked: true,
+                btnContectText: "Clicked"
+            })
+        }
+        else {
+            this.setState({
+                clicked: false,
+                btnContectText: "Contact Now"
+            })
+        }
+       
+    }
     render (){
         console.log("ContactItem component = ", this.props);
+
+        let btnContactName = "btn btn-default";
+
+        if (this.state.clicked){
+            btnContactName = "btn btn-danger"
+        }
+       
+        
         const sex = this.props.sex;
-        const url = `https://randomuser.me/api/portraits/${sex}/${this.props.avatar}.jpg`;
+        const url = `https://randomuser.me/api/portraits/${sex}/${this.state.avatar}.jpg`;
         const Style = {
             color: this.props.vip ? "green" : "pink",
             cursor: "pointer"
@@ -20,9 +58,10 @@ class ContactItem extends React.Component{
                 <p style={Style}> {this.props.contactDesc}</p>
                 
             </div>
-            <div className="media-right align-self-center">
-                <a href="#" className="btn btn-default">Contact Now</a>
+            <div className="media-right align-self-center" onClick={this.onContactClick}>
+                <a href="#" className={btnContactName}>{this.state.btnContectText}</a>
             </div>
+            <button type="button" className="btn btn-success" onClick={this.onAvatar}>Rand avatar</button>
         </div>
         )
     }
