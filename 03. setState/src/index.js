ddbeb694076605cf1cd4471eb100daee1244058a
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2019-09-03 19:20:26 +0300

    New features

--- 03. setState/src/index.js
+++ 03. setState/src/index.js
@@ -15,7 +15,7 @@ class App extends React.Component  {
     state = {
         List : [
             { id: 1, vip: false, sex: "women", avatar: 11 , contactName: "Camila Terry", contactDesc: "Camila Terry Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation" },
-            { id: 2, vip: false, sex: "men", avatar: 34 , contactName: "Bob Terry", contactDesc: "Bob Terry Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation" },
+            { id: 2, vip: true, sex: "men", avatar: 34 , contactName: "Bob Terry", contactDesc: "Bob Terry Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation" },
             { id: 3, vip: false, sex: "women", avatar: 72 , contactName: "Jesica Smith", contactDesc: "Jesica Smith Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation" },
             { id: 4, vip: false, sex: "men", avatar: 95 , contactName: "Jack Sparrow", contactDesc: "Jack Sparrow Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation" }
         ]
@@ -50,6 +50,10 @@ class App extends React.Component  {
 
     onFavorite = (id) => {
         console.log("Favorite =>  ", id);
+         const index = this.state.List.findIndex((elem) => elem.id === id);
+        console.log("vip => ", this.state.List[index].vip);
+        //Write code heare!
+        console.log("vip => ", this.state.List[index].vip);
     }
 
     addContact = () => {
